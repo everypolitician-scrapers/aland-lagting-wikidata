4905a287c98e57af9d263c566635651ab62fb337
Tony Bowden <tony@tmtm.com>
2016-03-19 07:33:07 +0100

    Also check FI wikipedia

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'pry'
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/aland-lagting-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { sv: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
+sv_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/aland-lagting-wikipedia', column: 'wikiname')
+fi_names = WikiData::Category.new( 'Luokka:Ahvenanmaalaiset poliitikot', 'fi').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { sv: sv_names, fi: fi_names }, output: false)
